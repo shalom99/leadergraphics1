ef2b4f9f1d10bb92872fa80cd8f92f91d3518b53
shalom99 <castroshalom77@gmail.com>
2023-07-22 00:48:50 +0800

    canvas pdf v1

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { FC } from 'react'
 
 type ProductsProps = {
@@ -10,8 +11,11 @@ const Products: FC<ProductsProps> = ({}) => {
     <>
     <div className='grid grid-cols-2 md:grid-cols-3 gap-x-2 lg:gap-x-10 gap-y-2'>
     <div className='flex flex-col items-center gap-2'>
+        <Link href="/designhub/designer">
+ 
         <p>Signboard</p>
         <Image src='/img_sample.jpg' alt="Logo" width={150} height={150} className='mb-5'/>
+        </Link>
     </div>
 
     <div className='flex flex-col items-center gap-2'>
